fe4a44800b599988d0d87a3502a648831984098f
Tristan <LeoDog896@hotmail.com>
2022-06-02 18:57:23 -0400

    Add collapsed value

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,7 @@
   interface Category {
     name: string;
     boxes: Box[];
+    collapsed?: boolean;
   }
 
   const categories: Category[] = [{
@@ -15,20 +16,29 @@
       {
         name: "Summnation",
         href: "boxes/summnation"
+      },
+      {
+        name: "Thing",
+        href: "boxes/thing"
       }
     ]
   }]
 </script>
 
 <div class="m-8">
-  {#each categories as { boxes, name }}
-    <h1 class="bg-gray-100 text-center rounded-lg p-6 text-2xl w-full">{name}</h1>
-    <div class="flex">
-      {#each boxes as { name, href }}
-      <a {href} class="flex flex-col bg-green-100 items-center justify-center w-1/4 p-4">
-        {name}
-      </a>
-      {/each}
-    </div>
+  {#each categories as { boxes, name, collapsed }}
+    <h1 on:click={() => collapsed = !collapsed} class="bg-gray-100 hover:bg-gray-200 active:bg-gray-300 transition-all cursor-pointer select-none text-center rounded-lg p-6 text-2xl w-full">
+      {name}
+      {collapsed ? ">" : "^"}
+    </h1>
+    {#if !!collapsed}
+      <div class="flex flex-wrap justify-around">
+        {#each boxes as { name, href }}
+        <a {href} class="flex flex-1 flex-col m-4 bg-green-100 hover:bg-green-200 active:bg-green-300 transition-all items-center justify-center w-1/4 p-4">
+          {name}
+        </a>
+        {/each}
+      </div>
+    {/if}
   {/each}
 </div>
