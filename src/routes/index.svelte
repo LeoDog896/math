591f7d09b7679b8c7b2dc71a07b23306893dfc2b
Tristan <LeoDog896@hotmail.com>
2022-06-02 16:23:06 -0400

    Full summnation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
+  import type { SvelteComponent } from "svelte";
+
   interface Box {
     name: string;
-    content: string;
+    href: string;
   }
 
   interface Category {
-    boxes: Box[]
+    name: string;
+    boxes: Box[];
   }
 
-  const categories: Category[] = []
+  const categories: Category[] = [{
+    name: "General",
+    boxes: [
+      {
+        name: "Summnation",
+        href: "boxes/summnation"
+      }
+    ]
+  }]
 </script>
+
+{#each categories as { boxes, name }}
+  {name}
+  {#each boxes as { name, href }}
+    {name}
+    <a {href}>{name}</a>
+  {/each}
+{/each}
