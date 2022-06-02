87db493ef40bc93f0c5e7c15d25482f6709cc387
Tristan <LeoDog896@hotmail.com>
2022-06-02 16:14:57 -0400

    Add mdsvex

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  interface Box {
+    name: string;
+    content: string;
+  }
+
+  interface Category {
+    boxes: Box[]
+  }
+
+  const categories: Category[] = []
+</script>
