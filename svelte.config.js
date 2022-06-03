5508121a81acefcbed598bf99b6a4f13e0570b28
Tristan <LeoDog896@hotmail.com>
2022-06-03 19:13:25 -0400

    Pu

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,7 +17,13 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: "/math"
+		},
+		prerender: {
+			default: true
+		}
 	}
 };
 
