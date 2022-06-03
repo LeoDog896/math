2e3bc2fd8e7f69990f30775226ce0e6fec13391a
Tristan <LeoDog896@hotmail.com>
2022-06-03 16:28:09 -0400

    Add syntax highlighting

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,9 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/themes/prism.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.6/dist/katex.min.css" integrity="sha384-ZPe7yZ91iWxYumsBEOn7ieg8q/o+qh/hQpSaPow8T6BwALcXSCS6C6fSRPIAnTQs" crossorigin="anonymous">
+</svelte:head>
+
 <slot />
