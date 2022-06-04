4146a8a1e534f2dc1e2fc039bad88916d022c788
Tristan <LeoDog896@hotmail.com>
2022-06-04 12:36:27 -0400

    Base styling, use suggestion
    Co-Authored-By: William Faircloth <willuhmjs@gmail.com>

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <div class="m-8">
+  <a href="/math">🠔 Back</a>
   <h1 class="text-3xl">{title}</h1>
   <slot></slot>
 </div>
--- src/lib/Result.svelte
+++ src/lib/Result.svelte
@@ -1,20 +1,32 @@
 <script>
   export let left = "Input"
   export let right = "Result"
+
+  export let equalWidth = true;
 </script>
 
-<div class="table w-full py-4 rounded-lg">
-  <div class="table-cell h-full border">
+<div class="flex items-stretch flex-row w-full py-4 rounded-lg">
+  <div class="flex-grow min-h-full border {equalWidth ? "w-1/2" : ""}">
     <div class="flex flex-col h-full">
-      <div class="w-full bg-gray-100 text-center text-xl p-4">{left}</div>
+      {#if left}
+        <div class="w-full bg-gray-100 text-center text-xl p-4">{left}</div>
+      {/if}
       <div class="p-4"><slot name="input"></slot></div>
     </div>
   </div>
 
-  <div class="table-cell h-full border">
-    <div class="flex flex-col h-full">
-      <div class="w-full bg-gray-100 text-center text-xl p-4">{right}</div>
+  <div class="flex-grow min-h-full border {equalWidth ? "w-1/2" : ""}">
+    <div class="flex flex-col h-full w-full">
+      {#if right}
+        <div class="w-full bg-gray-100 text-center text-xl p-4">{right}</div>
+      {/if}
       <div class="w-full flex flex-grow justify-center h-full items-center"><slot name="result"></slot></div>
     </div>
   </div>
 </div>
+
+<style>
+  :global(.katex) { 
+    @apply text-xl sm:text-3xl md:text-5xl lg:text-6xl xl:text-7xl;
+  }
+</style>
