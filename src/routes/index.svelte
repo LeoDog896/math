3bb32cba2b17f929a4a0cf51f23cafbe722d2f24
Tristan <LeoDog896@hotmail.com>
2022-06-04 15:38:31 -0400

    collapsed -> expanded

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,7 +9,7 @@
   interface Category {
     name: string;
     boxes: Box[];
-    collapsed?: boolean;
+    expanded?: boolean;
   }
 
   const categories: Category[] = [{
@@ -28,12 +28,12 @@
 </script>
 
 <div class="m-8">
-  {#each categories as { boxes, name, collapsed }}
-    <h1 on:click={() => collapsed = !collapsed} class="bg-gray-100 hover:bg-gray-200 active:bg-gray-300 transition-all cursor-pointer select-none text-center rounded-lg p-6 text-2xl w-full">
+  {#each categories as { boxes, name, expanded }}
+    <h1 on:click={() => expanded = !expanded} class="bg-gray-100 hover:bg-gray-200 active:bg-gray-300 transition-all cursor-pointer select-none text-center rounded-lg p-6 text-2xl w-full">
       {name}
-      {collapsed ? ">" : "^"}
+      {expanded ? "^" : ">"}
     </h1>
-    {#if !!collapsed}
+    {#if expanded}
       <div class="flex flex-wrap justify-around">
         {#each boxes as { name, href }}
         <a transition:fly={{ y: -20 }} {href} class="flex flex-1 flex-col m-4 bg-green-100 hover:bg-green-200 active:bg-green-300 transition-all items-center justify-center w-1/4 p-4">
