46924e4df0d2a9eee11ae241539ac4507d5838ae
Tristan <LeoDog896@hotmail.com>
2022-06-05 09:21:29 -0400

    Allow left/right ommitting in Result

--- src/lib/Result.svelte
+++ src/lib/Result.svelte
@@ -2,27 +2,36 @@
   export let left = "Input"
   export let right = "Result"
 
+  let leftExists = $$slots.input
+  let rightExists = $$slots.result
+
   export let equalWidth = true;
+
+  $: shouldUseEqualWidth = equalWidth && leftExists && rightExists
 </script>
 
 <div class="flex items-stretch flex-row w-full py-4 rounded-lg">
-  <div class="flex-grow min-h-full border {equalWidth ? "w-1/2" : ""}">
-    <div class="flex flex-col h-full">
-      {#if left}
-        <div class="w-full bg-gray-100 text-center text-xl p-4">{left}</div>
-      {/if}
-      <div class="p-4"><slot name="input"></slot></div>
+  {#if leftExists}
+    <div class="flex-grow min-h-full border {shouldUseEqualWidth ? "w-1/2" : "w-full"}">
+      <div class="flex flex-col h-full">
+        {#if left}
+          <div class="w-full bg-gray-100 text-center text-xl p-4">{left}</div>
+        {/if}
+        <div class="p-4"><slot name="input"></slot></div>
+      </div>
     </div>
-  </div>
+  {/if}
 
-  <div class="flex-grow min-h-full border {equalWidth ? "w-1/2" : ""}">
-    <div class="flex flex-col h-full w-full">
-      {#if right}
-        <div class="w-full bg-gray-100 text-center text-xl p-4">{right}</div>
-      {/if}
-      <div class="w-full flex flex-grow justify-center h-full items-center"><slot name="result"></slot></div>
+  {#if rightExists}
+    <div class="flex-grow min-h-full border {shouldUseEqualWidth ? "w-1/2" : "w-full"}">
+      <div class="flex flex-col h-full w-full">
+        {#if right}
+          <div class="w-full bg-gray-100 text-center text-xl p-4">{right}</div>
+        {/if}
+        <div class="w-full flex flex-grow justify-center h-full items-center"><slot name="result"></slot></div>
+      </div>
     </div>
-  </div>
+  {/if}
 </div>
 
 <style>
